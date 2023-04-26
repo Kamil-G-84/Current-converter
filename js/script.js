d1888a3e2e9930ebf0b0d4e4e569baa76c714953
Kamil Grzesik <kamil.grzesik84@gmail.com>
2023-04-26 23:00:12 +0200

    changed lines of code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 {
     const calculateResult = (amount, currency) => {
-
         const currencyDolar = 4.16;
         const currencyEuro = 4.59;
         const currencyPeso = 0.00092;
@@ -17,26 +16,28 @@
         }
     };
 
+    const updateResultText = (amount, currency, result) => {
+        const resultElement = document.querySelector(".js-result");
+        resultElement.innerHTML = `${amount.toFixed(2)} PLN = ${result.toFixed(2)} ${currency}`;
+    }
+
     const onFormSubmit = (event) => {
         event.preventDefault();
 
         const amountElement = document.querySelector(".js-amount");
         const currencyElement = document.querySelector(".js-currency");
-        const resultElement = document.querySelector(".js-result");
 
         const amount = +amountElement.value;
         const currency = currencyElement.value;
 
         const result = calculateResult(amount, currency);
 
-        resultElement.innerHTML = `${amount.toFixed(2)} PLN = ${result.toFixed(2)} ${currency}`;
-
+        updateResultText = (amount, currency, result)
     };
 
     const init = () => {
 
         const formElement = document.querySelector(".js-form");
-
         formElement.addEventListener("submit", onFormSubmit)
 
     };
