625aceb97bdc1d5c082f88fa9ff7b195607ea940
Kamil Grzesik <kamil.grzesik84@gmail.com>
2023-04-26 22:39:06 +0200

    improved the correctness of the code

--- js/script.js
+++ js/script.js
@@ -17,28 +17,30 @@
         }
     };
 
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const amountElement = document.querySelector(".js-amount");
+        const currencyElement = document.querySelector(".js-currency");
+        const resultElement = document.querySelector(".js-result");
+
+        const amount = +amountElement.value;
+        const currency = currencyElement.value;
+
+        const result = calculateResult(amount, currency);
+
+        resultElement.innerHTML = `${amount.toFixed(2)} PLN = ${result.toFixed(2)} ${currency}`;
+
+    };
+
     const init = () => {
 
         const formElement = document.querySelector(".js-form");
 
-        formElement.addEventListener("submit", (event) => {
-            event.preventDefault();
+        formElement.addEventListener("submit", onFormSubmit)
 
-            const amountElement = document.querySelector(".js-amount");
-            const currencyElement = document.querySelector(".js-currency");
-            const resultElement = document.querySelector(".js-result");
+    };
 
-            const amount = +amountElement.value;
-            const currency = currencyElement.value;
-           
-            const result = calculateResult(amount, currency);
-
-            resultElement.innerHTML = `${amount.toFixed(2)} PLN = ${result.toFixed(2)} ${currency}`;
-        
-    });
-    
-};
-
-init();
+    init();
 
 }
